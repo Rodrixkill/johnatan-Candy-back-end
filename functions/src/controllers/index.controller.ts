9ca23970cfbb136fe1b168152ed187bf9117aee4
Samuel150 <hpsamuel150@gmail.com>
2021-05-09 20:49:11 -0400

    seperate build and deploy

--- functions/src/controllers/index.controller.ts
+++ functions/src/controllers/index.controller.ts
@@ -6,7 +6,7 @@ import { Usuario } from '../interface/Usuario'
 const SECRET_KEY_HERE="jhonatanCandy";
 
 export async function test(req: Request, res: Response):Promise<any>{
-   res.send("Funciona el pipeline editado")
+   res.send("Funciona CI/CD")
 }
 
 export async function indexWelcome(req: Request, res: Response): Promise<any> {
